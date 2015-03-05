899db9fb2b50f0c2c975825ed1eee7c1c1a8fa5e
Randal Direen <rdireen@direentech.com>
2015-03-04 18:14:03 -0700

    Trying again to get appveyor quicker, see logs

--- appveyor/install_numpy.ps1
+++ appveyor/install_numpy.ps1
@@ -10,7 +10,7 @@ function InstallNumpy(){
     if (-not(Test-Path "c:\tmp\*.whl")) {
         Write-Host "numpy has not been compiled yet. Starting Long process..."
         Write-Host "pip wheel --wheel-dir=c:\tmp\ numpy"
-        iex "cmd /E:ON /V:ON /C .\\appveyor\\run_with_env.cmd 'pip wheel --wheel-dir=c:\\tmp numpy'"
+        iex "cmd /E:ON /V:ON /C .\\appveyor\\run_with_env.cmd pip wheel --wheel-dir=c:\\tmp numpy"
     } else {
         Write-Host "numpy has already been compiled."
         Get-ChildItem "C:\tmp"
